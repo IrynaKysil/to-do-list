e9044b4e69be472c029b2c8f69b08f5512c7b6f0
henok <babrdy@gmail.com>
2021-09-24 08:01:20 -0600

    I made changes

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const newTaskNameInput = document.querySelector('#taskName');
+const taskName = newTaskNameInput.value;
+console.log("name:  "+ taskName);
+
+const newTaskDescription = document.querySelector('#taskDescription');
+const taskDescription = newTaskDescription.value;
+console.log("description:  "+ taskDescription);
+
+const newAssignedTo = document.querySelector('#assignedTo');
+const assigneTo = newAssignedTo.value;
+console.log("assigne:  "+ assigneTo);
+
