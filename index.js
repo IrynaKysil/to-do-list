ae2e263244adb8c80c454c0b033c8240943741af
henok <babrdy@gmail.com>
2021-09-30 08:18:42 -0600

    amendments

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
+
+
+document.querySelector('#studentform').addEventListener("change", validFormFieldInput)
+
+function validFormFieldInput(data) {
+console.log(data);
+
+
 const newTaskNameInput = document.querySelector('#taskName');
 const taskName = newTaskNameInput.value;
 console.log("name:  "+ taskName);
@@ -10,4 +18,11 @@ console.log("description:  "+ taskDescription);
 const newAssignedTo = document.querySelector('#assignedTo');
 const assigneTo = newAssignedTo.value;
 console.log("assigne:  "+ assigneTo);
+
+
+const newDueDate = document.querySelector('#dueDate');
+const dueDate = newDueDate.value;g
+console.log("due date:  " + dueDate);
+}
+
 
